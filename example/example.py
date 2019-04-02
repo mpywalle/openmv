59ae3628b43de4063cfe4ae162be4bf934c025f3
xiabizha <robin1002@163.com>
2019-04-03 00:08:07 +0800

    add example.py, and update mpy for KT device exception handling

---
+++ example/example.py
@@ -0,0 +1,52 @@
+import pyb
+import time
+import _thread
+from Sensor import TP, Button
+from Driver import DMotor, LED
+	
+def led(id, dt):
+	while 1:
+		pyb.LED(id).toggle()
+		time.sleep(dt)
+        
+def real_time_temp(n, dt):
+	temp = TP()
+	cnt = 0
+	while 1:
+		val = temp.get_temp()
+		time.sleep(dt)
+		print(cnt, val)
+		cnt = cnt + 1
+
+def run_motor(n, dt):
+	motor = DMotor()
+	while 1:
+		val = motor.set_speed(45, 1, 45, 1)
+		time.sleep(dt)
+		val = motor.set_speed(45, 2, 45, 2)
+		time.sleep(dt)
+		
+def exled(id, dt):
+	led = LED()
+	while 1:
+		led.set_color(80, 0, 0)
+		time.sleep(dt)
+		led.set_color(0, 80, 0)
+		time.sleep(dt)
+		led.set_color(0, 0, 80)
+		time.sleep(dt)
+
+def get_button(id, dt):
+	but = Button()
+	while 1:
+		status = but.get_status()
+		print(status)
+		time.sleep(dt)
+
+time.sleep(2000)
+#get_button(3, 5000)
+#led(1, 1000)
+#real_time_temp(1, 1000)
+#run_motor(1, 1000)
+exled(3, 200)
+#get_button(3, 5000)
